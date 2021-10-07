983963921ca5d1a50edcc53381d140aee05d39a0
Rodrigo Calmon <89034960+rodrigoccalmon@users.noreply.github.com>
2021-10-07 14:28:37 -0300

    Add files via upload

---
+++ aula09/src/aula09/Aluno.java
@@ -0,0 +1,46 @@
+package aula09;
+
+public class Aluno {
+	private String nome;
+	private double nota1;
+	private double nota2;
+	public Aluno(String nome, double nota1, double nota2) {
+		super();
+		this.nome = nome;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double calcularMedia(double a, double b) throws IllegalArgumentException{
+		a = this.nota1;
+		b = this.nota2;
+		double media = (a+b) / 2;
+		if (b == 0 || a == 0) {
+			throw new IllegalArgumentException("Argumento ilegal. número não pode ser 0.");
+		}
+		System.out.println("Média: "+media);
+		return media;	
+	}
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", nota1=" + nota1 + ", nota2=" + nota2 + "]";
+	}
+	
+}
